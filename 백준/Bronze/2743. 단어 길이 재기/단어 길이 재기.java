aa6006a01a8299a1e8aa5b0d40ef64cd238bade6
JAESEONGIM <129404679+JAESEONGIM@users.noreply.github.com>
2024-04-15 17:54:15 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 108 ms, Memory: 12760 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s;
+		Scanner sc = new Scanner(System.in);
+		s = sc.next();
+		int a = s.length();
+		System.out.println(a);
+	}
+}

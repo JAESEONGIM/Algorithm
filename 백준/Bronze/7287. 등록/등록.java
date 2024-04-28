57f2be85b32ff25836ad35e400037e7ff7a42146
JAESEONGIM <129404679+JAESEONGIM@users.noreply.github.com>
2024-04-28 19:17:01 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11312 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String args[]){
+        
+        System.out.println("77");
+        System.out.println("bigstar2878");
+        
+        
+    }
+    
+    
+    
+}

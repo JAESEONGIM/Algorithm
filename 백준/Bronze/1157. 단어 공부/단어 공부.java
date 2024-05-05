e49677e31990802d1f5bb93aa677b951e14471d3
JAESEONGIM <129404679+JAESEONGIM@users.noreply.github.com>
2024-05-05 23:17:44 +0900

    [Bronze I] Title: 단어 공부, Time: 388 ms, Memory: 32772 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,43 +1,36 @@
-
-
-import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		HashMap<Character, Integer> map = new HashMap<>();
-		String s = sc.next().toUpperCase();
-		//문자열 입력받기
-		//char[] arr = new char[s.length()];
-		// 문자열을 문자단위로 떼어서 넣을 문자형 배열 선언
-		for(int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			map.put(c, map.getOrDefault(c, 0)+1);
-		}
-		int maxv = 0;
-		Character maxk = null;
-		boolean multipleMax =false;
-		for(Entry<Character, Integer> entry : map.entrySet()) {
-			//if()
-			if(entry.getValue() > maxv) {
-				maxv=entry.getValue();
-				maxk = entry.getKey();
-				 multipleMax = false;
-			}else if(entry.getValue() == maxv) {
-				multipleMax = true;
-			}
-		}
-			if(multipleMax) {
-				System.out.println("?");
-			}else {
-				System.out.println(maxk);
-			}
-		}
-		
-		
-		}
-	
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String s2 = s.toUpperCase();
+        int max = 0;
+        int[] count = new int[26];
+        int maxindex =0;
+        char maxalphabet = '?';
+        for (int i = 0; i < s2.length(); i++){
+            char ch = s2.charAt(i);
+            count[ch-'A']++;
+
+        }
+        for (int i = 0 ; i < 26; i++){
+            if (max < count[i]){
+                max = count[i];
+                maxalphabet = (char)('A'+ i);
+            } else if (count[i] == max) {
+                maxalphabet = '?';
+            }
+        }
+        System.out.println((char)(maxalphabet));
+
+
+
+
+
+
+    }
+}
